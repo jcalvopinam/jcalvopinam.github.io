bef1d7a6dd43e35114e6e75a45b6343fcb109718
Juan Calvopina <juan.calvopina@gmail.com>
2025-10-27 18:32:44 -0500

    Include svg animation and blog detail

--- themes/uno/assets/js/main.js
+++ themes/uno/assets/js/main.js
@@ -1,35 +1,55 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  var animationTime = 400;
+  var $blogButton = $('a.blog-button');
+  var $panelCover = $('.panel-cover');
 
   $('body').removeClass('no-js');
 
-  $('a.blog-button').click(function() {
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
-    currentWidth = $('.panel-cover').width();
+  $blogButton.click(function () {
+    if ($panelCover.hasClass('panel-cover--collapsed')) return;
+    currentWidth = $panelCover.width();
     if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed');
+      $panelCover.addClass('panel-cover--collapsed');
       $('.content-wrapper').addClass('animated slideInRight');
     } else {
-      $('.panel-cover').css('max-width',currentWidth);
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function() {} );
+      $panelCover.css('max-width', currentWidth);
+      $panelCover.animate({ 'max-width': '530px', 'width': '40%' }, animationTime, swing = 'swing', function () { });
     }
   });
 
   if (window.location.hash && window.location.hash == "#blog") {
-    $('.panel-cover').addClass('panel-cover--collapsed');
+    $blogButton.find('span').text('❮❮');
+    $panelCover.addClass('panel-cover--collapsed');
   }
 
   if (window.location.pathname.substring(0, 5) == "/tag/") {
-    $('.panel-cover').addClass('panel-cover--collapsed');
+    $panelCover.addClass('panel-cover--collapsed');
   }
 
-  $('.btn-mobile-menu').click(function() {
+  $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
   });
 
-  $('.navigation-wrapper .blog-button').click(function() {
+  $('.navigation-wrapper .blog-button').click(function () {
     $('.navigation-wrapper').toggleClass('visible');
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
+  });
+
+  $blogButton.on('click', function (e) {
+    if (window.location.hash === '#blog') {
+      // open > close
+      e.preventDefault();
+      window.location.hash = ''; // remove hash
+      $blogButton.find('span').text('❯❯');
+      $panelCover.removeClass('panel-cover--collapsed');
+      $panelCover.removeAttr('style'); // clean style inline width
+      $panelCover.animate({ 'max-width': '100%' }, animationTime, swing = 'swing', function () { });
+    } else {
+      // close > open
+      $blogButton.find('span').text('❮❮');
+      $panelCover.addClass('panel-cover--collapsed');
+    }
   });
 
 });
